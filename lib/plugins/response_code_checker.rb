84fb0f55334e1a9f3d1a7c84a0557c256676929e
Jan Roesner <jan@roesner.it>
2010-10-04 09:58:39 +0200

    Improved stability of all checkers introducing rescue blocks in case of exceptional errors. Signed-off-by: Jan Roesner <jan@roesner.it>

--- lib/plugins/response_code_checker.rb
+++ lib/plugins/response_code_checker.rb
@@ -3,9 +3,14 @@ class ResponseCodeChecker < Checker
   # checks the given responses status code against the one in the expectation and returns result object
   def check
     result = Result.new(@testcase, @response)
-    if not @testcase.response_expectation['status_code'].to_s == @response.code.to_s
+    begin
+      if not @testcase.response_expectation['status_code'].to_s == @response.code.to_s
+        result.succeeded = false
+        result.error_message = " expected response code --#{@testcase.response_expectation['status_code']}--\n got response code --#{@response.code}--"
+      end
+    rescue
       result.succeeded = false
-      result.error_message = " expected response code --#{@testcase.response_expectation['status_code']}--\n got response code --#{@response.code}--"
+      result.error_message = " unexpected error while parsing testcase/response. Check your testcase format!"
     end
     result
   end
--- lib/plugins/response_header_checker.rb
+++ lib/plugins/response_header_checker.rb
@@ -3,19 +3,24 @@ class ResponseHeaderChecker < Checker
   # checks given header against the given expepctation and returns a result object
   def check
     result = Result.new(@testcase, @response)
-    @testcase.response_expectation['headers'].each_pair do |header_name, header_value|
-      if is_regex?(header_value)
-        if not (excluded?(header_name) or regex_matches?(header_value, @response.headers[header_name]))
-          result.succeeded = false
-          result.error_message = " expected header identifier --#{header_name}-- to match regex --#{header_value}--\n got --#{@response.headers[header_name]}--"
+    begin
+      @testcase.response_expectation['headers'].each_pair do |header_name, header_value|
+        if is_regex?(header_value)
+          if not (excluded?(header_name) or regex_matches?(header_value, @response.headers[header_name]))
+            result.succeeded = false
+            result.error_message = " expected header identifier --#{header_name}-- to match regex --#{header_value}--\n got --#{@response.headers[header_name]}--"
+          end
+        else
+          if not (excluded?(header_name) or string_matches?(header_value, @response.headers[header_name]))
+            result.succeeded = false
+            result.error_message = " expected header identifier --#{header_name}-- to match --#{header_value}--\n got --#{@response.headers[header_name]}--"
+          end
         end
-      else
-        if not (excluded?(header_name) or string_matches?(header_value, @response.headers[header_name]))
-          result.succeeded = false
-          result.error_message = " expected header identifier --#{header_name}-- to match --#{header_value}--\n got --#{@response.headers[header_name]}--"
-        end
-      end
-    end unless (@testcase.response_expectation['headers'].nil? or @testcase.response_expectation['headers'].empty?)
+      end unless (@testcase.response_expectation['headers'].nil? or @testcase.response_expectation['headers'].empty?)
+    rescue
+      result.succeeded = false
+      result.error_message = " unexpected error while parsing testcase/response. Check your testcase format!"
+    end
     result
   end
 
