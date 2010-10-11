7d965ac670341fadc25eb46bf8c6ff8d804b0529
Jan Roesner <jan@roesner.it>
2010-10-11 17:59:27 +0200

    Tried to fix String extension. Signed-off-by: Jan Roesner <jan@roesner.it>

--- lib/string_ext.rb
+++ lib/string_ext.rb
@@ -5,11 +5,19 @@ class String
   end
 
   # opposites underscore defined above
-  def camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
+  def camelize(first_letter_in_uppercase = true)
     if first_letter_in_uppercase
-      lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+      self.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
     else
-      lower_case_and_underscored_word.first + camelize(lower_case_and_underscored_word)[1..-1]
+      self.first + camelize(lower_case_and_underscored_word)[1..-1]
     end
   end
+  # # opposites underscore defined above
+  # def camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
+  #   if first_letter_in_uppercase
+  #     lower_case_and_underscored_word.to_s.gsub(/\\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
+  #   else
+  #     lower_case_and_underscored_word.first + camelize(lower_case_and_underscored_word)[1..-1]
+  #   end
+  # end
 end
